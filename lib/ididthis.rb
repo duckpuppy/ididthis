506fd67e05a7a6237193ac08382c5fcc68505a51
Patrick Aikens <patrick.aikens@manheim.com>
2015-07-23 11:40:34 -0400

    Created config class

--- lib/ididthis.rb
+++ lib/ididthis.rb
@@ -1,5 +1,13 @@
+#!/usr/bin/env ruby
+
 require "ididthis/version"
+require "net/http"
+require "highline/import"
+require "json"
 
 module Ididthis
-  # Your code goes here...
+  CONFIG_FILE = File.expand_path("~/.ididthis")
+  API_VERSION = "0.0"
+  API_ROOT = "https://idonethis.com/api/v#{API_VERSION}"
+  $endpoints = {}
 end
---
+++ lib/ididthis/config.rb
@@ -0,0 +1,5 @@
+module Ididthis
+  class Config
+    # TODO Configuration related items go here, like creating defaults and loading from user config
+  end
+end
