138792a88675890d702bd5035e26daa6b2d0911a
Patrick Aikens <patrick.aikens@manheim.com>
2015-08-03 17:04:05 -0400

    Basic functionality works

--- lib/ididthis.rb
+++ lib/ididthis.rb
@@ -1,13 +1,18 @@
 #!/usr/bin/env ruby
 
 require "ididthis/version"
-require "net/http"
+require "ididthis/config"
+require "ididthis/api"
 require "highline/import"
 require "json"
 
 module Ididthis
   CONFIG_FILE = File.expand_path("~/.ididthis")
-  API_VERSION = "0.0"
+  API_VERSION = "0.1"
   API_ROOT = "https://idonethis.com/api/v#{API_VERSION}"
-  $endpoints = {}
+
+  ENDPOINTS = {}
+  ENDPOINTS[:team] = "#{API_ROOT}/teams/"
+  ENDPOINTS[:dones] = "#{API_ROOT}/dones/"
+  ENDPOINTS[:noop] = "#{API_ROOT}/noop/"
 end
--- lib/ididthis/config.rb
+++ lib/ididthis/config.rb
@@ -1,5 +1,62 @@
+require 'highline/import'
+require 'yaml'
+
 module Ididthis
-  class Config
-    # TODO Configuration related items go here, like creating defaults and loading from user config
+  module Config
+    extend self
+    PATH = ENV['IDIDTHIS_CONFIG_FILE'] || File.join(ENV['HOME'], '.ididthis.yml')
+
+    def defaults
+      {
+        team: '',
+        token: ''
+      }
+    end
+
+    def [](key)
+      configs[key]
+    rescue => e
+      warn 'invalid config file'
+      warn e.message
+      defaults[key]
+    end
+
+    def configure!
+      current = configs
+      client = Ididthis::API::Client.new
+
+      token = ask("iDoneThis API Token: ") do |token| 
+        token.default = current[:token]
+        token.whitespace = :strip
+        token.responses[:not_valid] = "The token you have entered cannot be validated.  Please try again."
+        token.responses[:ask_on_error] = :question
+        token.validate = lambda do |t|
+          client.validate_token(t)
+        end
+      end
+
+      teams = client.get_teams(token)
+      team = choose do |menu|
+        menu.prompt = "Which team will you be posting dones to? "
+        menu.select_by = :index_or_name
+        teams.each do |team|
+          menu.choice(team[:name]) {
+            team
+          }
+        end
+      end
+
+      File.open(PATH, 'w') do |f|
+        f << { :token => token, :team => team[:short_name] }.to_yaml
+      end
+    end
+
+    def configs
+      if File.exist?(PATH)
+        defaults.merge(YAML.load_file(PATH))
+      else
+        defaults
+      end
+    end
   end
 end
