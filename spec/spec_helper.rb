dbf5c881ce52ca66f6c9e43e528cbc483d3dc41b
Patrick Aikens <patrick.aikens@manheim.com>
2015-08-05 13:21:18 -0400

    Use HTML output as well as Coveralls output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,20 @@
+$TESTING = true
+
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  add_filter "/spec/"
+  minimum_coverage(50.0)
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ididthis'
 require 'dotenv'
 Dotenv.load
 
-require 'coveralls'
-Coveralls.wear!
