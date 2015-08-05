bd1d03d750fc83dc72159f54d883f609d19ac31e
Patrick Aikens <patrick.aikens@manheim.com>
2015-08-05 13:53:35 -0400

    Use .simplecov in preparation for test result merging

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,10 @@
 $TESTING = true
 
+require 'dotenv'
+Dotenv.load
+
 require 'simplecov'
-require 'coveralls'
-require 'codeclimate-test-reporter'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter,
-  CodeClimate::TestReporter::Formatter
-]
-
-SimpleCov.start do
-  add_filter "/spec/"
-  minimum_coverage(50.0)
-end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ididthis'
-require 'dotenv'
-Dotenv.load
 
